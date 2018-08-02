bade42466f36caca10c4cb874ba1ed2bffe624b1
Amrita Krishna <amritakrishna12@gmail.com>
2018-08-02 17:56:10 +0530

    Updated PrimeFactorsTest.java

--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -1,14 +1,4 @@
-import org.junit.Ignore;
-import org.junit.Test;
-
 import java.util.ArrayList;
-import java.util.Collections;
-
-import static org.junit.Assert.*;
-
-import static java.util.Arrays.asList;
-
-
 
 public class PrimeFactorsTest {
 
